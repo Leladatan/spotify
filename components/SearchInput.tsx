e20c1aa4d4652071a83c0bb069eda0f4a61b5b44
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-06-28 19:15:03 +0300

    added SearchInput component

---
+++ components/SearchInput.tsx
@@ -0,0 +1,35 @@
+"use client"
+import qs from "query-string";
+import React, {FC, useEffect, useState} from 'react';
+import {useRouter} from "next/navigation";
+import useDebounce from "@/hooks/useDebounce";
+import Input from "@/components/Input";
+
+const SearchInput: FC = () => {
+    const router = useRouter();
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500);
+
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        };
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query,
+        });
+
+        router.push(url);
+    }, [debouncedValue, router]);
+
+    return (
+        <Input
+            placeholder="What do you want to listen to ?"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+    );
+};
+
+export default SearchInput;
