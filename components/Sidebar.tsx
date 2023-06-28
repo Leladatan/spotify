405076bdaebb9e21fc04412dd65bcca87a60d684
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-06-28 18:40:08 +0300

    added Sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, {FC, useMemo} from 'react';
+import {usePathname} from "next/navigation";
+import {HiHome, HiSearch} from "react-icons/hi";
+import Box from "@/components/Box";
+import SidebarItem from "@/components/SidebarItem";
+import Library from "@/components/Library";
+import {Song} from "@/types";
+interface SidebarProps {
+    children: React.ReactNode;
+    songs: Song[];
+}
+const Sidebar: FC<SidebarProps> = ({children, songs}) => {
+
+    const pathname = usePathname();
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {
+            icon: HiSearch,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search',
+        }
+    ], [pathname]);
+
+    return (
+        <div className="flex h-full">
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                    <div className="
+                        flex
+                        flex-col
+                        gap-y-4
+                        px-5
+                        py-4
+                    ">
+                        {routes.map((item) => (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library songs={songs}/>
+                </Box>
+            </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default Sidebar;
