dd795891ccbb58f9407dd84da94181d0996e2ec0
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-06-28 20:40:40 +0300

    refactor code Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,4 @@
-import React, {FC, forwardRef} from 'react';
+import React, {forwardRef} from 'react';
 import {twMerge} from "tailwind-merge";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
