4068dcfdab90844daa029f2cf984f7ca8eeff7c3
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-06-28 19:16:47 +0300

    added Home page

---
+++ app/(site)/components/SongsContent.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, {FC} from 'react';
+import {Song} from "@/types";
+import SongItem from "@/components/SongItem";
+
+interface SongsContentProps {
+    songs: Song[];
+}
+
+const SongsContent: FC<SongsContentProps> = ({songs}) => {
+    if (songs.length === 0) {
+        return (
+            <div className="mt-4 text-neutral-400">
+                No songs available.
+            </div>
+        )
+    }
+    return (
+        <div
+            className="
+            grid
+            grid-cols-2
+            sm:grid-cols-3
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-4
+            mt-4
+        ">
+            {songs.map(song => (
+                <SongItem
+                    key={song.id}
+                    onClick={() => {}}
+                    data={song}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default SongsContent;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,33 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+import {NextPage} from "next";
+import getSongs from "@/actions/getSongs";
+import SongsContent from "@/app/(site)/components/SongsContent";
 
-const HomePage = () => {
+export const revalidate = 0;
+const HomePage:NextPage = async () => {
+    const songs = await getSongs();
+
   return (
-      <div className="text-green-500 ">
-        Hello (Site)!
+      <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+          <Header>
+              <div className="mb-2">
+                  <h1 className="text-white text-3xl font-semibold">
+                    Welcome back
+                  </h1>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4 ">
+                    <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
+                  </div>
+              </div>
+          </Header>
+          <main className="mt-2 mb-7 px-6">
+              <section className="flex justify-between items-center">
+                  <h2 className="text-white text-2xl font-semibold">
+                      Newest Songs
+                  </h2>
+              </section>
+              <SongsContent songs={songs}/>
+          </main>
       </div>
   )
 }
