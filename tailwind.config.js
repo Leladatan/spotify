04087091c67d25850c0410f0377678a1c60e2eec
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-06 16:22:06 +0300

    added screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
     extend: {
 
     },
+    screens: {
+      '2sm': '422px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
