6ec2c81194ded748ac71aecc6b91cc9f537c7420
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 13:50:38 +0300

    update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import AccountContent from "./components/AccountContent";
 import {NextPage} from "next";
 import Header from "@/components/Header";
-import Image from "next/image";
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,57 +11,21 @@ export const metadata: Metadata = {
 const AccountPage: NextPage = () => {
     return (
         <div className="
-            bg-neutral-900
-            rounded-lg
-            h-full
-            w-full
-            overflow-hidden
+            bg-neutral-900 
+            rounded-lg 
+            h-full 
+            w-full 
+            overflow-hidden 
             overflow-y-auto
-            scrollbar-thin
         ">
-            <Header>
-                <div className="mt-20">
-                    <div className="
-                        flex
-                        flex-col
-                        md:flex-row
-                        items-center
-                        gap-x-5
-                    ">
-                        <div className="
-                            relative
-                            h-32
-                            w-32
-                            lg:h-40
-                            lg:w-40
-                        ">
-                            <Image fill alt="Playlist" src="/images/liked.png" className="object-cover rounded-lg" />
-                        </div>
-                        <div className="
-                            flex
-                            flex-col
-                            gap-y-2
-                            mt-4
-                            md:mt-0
-                        ">
-                            <p className="
-                                hidden md:block font-semibold text-3xl
-                            ">
-                                Profile
-                            </p>
-                            <h1 className="
-                                text-white
-                                text-4xl
-                                sm:text-5xl
-                                lg:text-7xl
-                                font-bold
-                            ">
-                                Profile
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-            </Header>
+          <Header className="from-bg-neutral-900">
+            <div className="mb-2 flex flex-col gap-y-6">
+              <h1 className="text-white text-3xl font-semibold">
+                Account Settings
+              </h1>
+            </div>
+          </Header>
+          <AccountContent />
         </div>
     );
 };
