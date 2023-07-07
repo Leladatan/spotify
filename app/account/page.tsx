66607127d74a555e8930d571e6ca43cecfd11f7a
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 13:52:14 +0300

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,4 +1,4 @@
-import AccountContent from "./components/AccountContent";
+import AccountContent from "./AccountContent";
 import {NextPage} from "next";
 import Header from "@/components/Header";
 import type { Metadata } from 'next';
