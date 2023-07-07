af7babc9f939f850f9378ad10dbd13461cec60ea
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 13:53:26 +0300

    Update AccountContent.tsx

--- app/account/AccountContent.tsx
+++ app/account/AccountContent.tsx
@@ -1,18 +1,12 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
 import { useUser } from "@/hooks/useUser";
 import Button from "@/components/Button";
-import useSubscribeModal from "@/hooks/useSubscribeModal";
-import { postData } from "@/libs/helpers";
 
 const AccountContent = () => {
   const router = useRouter();
-  const subscribeModal = useSubscribeModal();
   const { isLoading, subscription, user } = useUser();
-
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -20,19 +14,6 @@ const AccountContent = () => {
       router.replace('/');
     }
   }, [isLoading, user, router]);
-
-  const redirectToCustomerPortal = async () => {
-    setLoading(true);
-    try {
-      const { url, error } = await postData({
-        url: '/api/create-portal-link'
-      });
-      window.location.assign(url);
-    } catch (error) {
-      if (error) return alert((error as Error).message);
-    }
-    setLoading(false);
-  };
 
   return ( 
     <div className="mb-7 px-6">
