a90bc76fb3fa90bb726e5b893366a52af03c5671
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 13:13:48 +0300

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,6 @@ import Box from "@/components/Box";
 import SidebarItem from "@/components/SidebarItem";
 import Library from "@/components/Library";
 import {Song} from "@/types";
-import {useUser} from "@/hooks/useUser";
 import usePlayer from "@/hooks/usePlayer";
 import {twMerge} from "tailwind-merge";
 interface SidebarProps {
@@ -16,7 +15,6 @@ interface SidebarProps {
 const Sidebar: FC<SidebarProps> = ({children, songs}) => {
     const pathname = usePathname();
     const player = usePlayer();
-    const {isLoading, user} = useUser();
     const routes = useMemo(() => [
         {
             icon: HiHome,
@@ -33,7 +31,7 @@ const Sidebar: FC<SidebarProps> = ({children, songs}) => {
     ], [pathname]);
 
     return (
-        <div className={twMerge(`flex h-full`, (player.activeId || isLoading && user) && "h-[calc(100%-130px)]")}>
+        <div className={twMerge(`flex h-full`, player.activeId  && "h-[calc(100%-130px)]")}>
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
                     <div className="
