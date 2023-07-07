8ee4c572d1f12a35afaff526c2dc611676410ec1
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 12:06:36 +0300

    Update SearchContent.tsx

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -3,6 +3,7 @@ import React, {FC} from 'react';
 import {Song} from "@/types";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
+import {useUser} from "@/hooks/useUser";
 import useOnPlay from "@/hooks/useOnPlay";
 import {twMerge} from "tailwind-merge";
 import usePlayer from "@/hooks/usePlayer";
@@ -13,6 +14,7 @@ interface SearchContent {
 
 const SearchContent: FC<SearchContent> = ({songs}) => {
     const player = usePlayer();
+    const {isLoading, user} = useUser();
     const onPlay = useOnPlay(songs);
 
     if (songs.length === 0) {
@@ -24,7 +26,7 @@ const SearchContent: FC<SearchContent> = ({songs}) => {
     }
 
     return (
-        <div className={twMerge(`flex flex-col gap-y-2 w-full px-6 h-full`, player.activeId && "h-[calc(100%-130px)]")}>
+        <div className={twMerge(`flex flex-col gap-y-2 w-full px-6 h-full`, (player.activeId || isLoading && user) && "h-[calc(100%-130px)]")}>
             {songs.map(song => (
                 <div
                     key={song.id}
