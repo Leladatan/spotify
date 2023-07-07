d1192a0d9045806f41c8ed7e176da47fd14544a9
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 12:20:00 +0300

    Update SearchContent.tsx

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -3,6 +3,8 @@ import React, {FC} from 'react';
 import {Song} from "@/types";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import {useUser} from "@/hooks/useUser";
 import useOnPlay from "@/hooks/useOnPlay";
 import {twMerge} from "tailwind-merge";
@@ -14,7 +16,9 @@ interface SearchContent {
 
 const SearchContent: FC<SearchContent> = ({songs}) => {
     const player = usePlayer();
+    const {song} = useGetSongById(String(player.activeId));
     const {isLoading, user} = useUser();
+    const songUrl = useLoadSongUrl(song!);
     const onPlay = useOnPlay(songs);
 
     if (songs.length === 0) {
@@ -26,7 +30,7 @@ const SearchContent: FC<SearchContent> = ({songs}) => {
     }
 
     return (
-        <div className={twMerge(`flex flex-col gap-y-2 w-full px-6 h-full`, (player.activeId || isLoading && user) && "h-[calc(100%-130px)]")}>
+        <div className={twMerge(`flex flex-col gap-y-2 w-full px-6 h-full`, (song || songUrl || player.activeId || (isLoading && user)) && "h-[calc(100%-130px)]")}>
             {songs.map(song => (
                 <div
                     key={song.id}
