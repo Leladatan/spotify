744688a5779726ca7bd196a3635ec2110ffc5a5f
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-07 12:45:09 +0300

    Update Player.tsx

--- components/Player.tsx
+++ components/Player.tsx
@@ -12,7 +12,7 @@ const Player: FC = () => {
     const {isLoading, user} = useUser();
     const songUrl = useLoadSongUrl(song!);
 
-    console.log(!song || !songUrl || !player.activeId || (!isLoading && !user): "player");
+    console.log(!song || !songUrl || !player.activeId || (!isLoading && !user));
 
     if (!song || !songUrl || !player.activeId || (!isLoading && !user)) {
         return null;
