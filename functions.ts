c9f2216e467e1e899d086f716edb53834ac8c2f2
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-10 15:47:51 +0300

    Update functions.ts

--- functions.ts
+++ functions.ts
@@ -28,12 +28,3 @@ export const translit = (word: string): string => {
 
     return answer;
 }
-
-export const debounce = (callback: any, delay: number): any => {
-    let timeoutId;
-
-    return () => {
-        clearTimeOut(timeoutId);
-        timeoutId = setTimeout(callback, delay);
-    }
-}
