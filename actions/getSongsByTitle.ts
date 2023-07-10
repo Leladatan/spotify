3d9836c228554a13d81f239be78502c4d7512a29
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-10 13:57:26 +0300

    Update getSongsByTitle.ts

--- actions/getSongsByTitle.ts
+++ actions/getSongsByTitle.ts
@@ -15,7 +15,7 @@ const getSongsByTitle = async (title: string): Promise<Song[]> => {
     const {data, error} = await supabase
         .from('songs')
         .select('*')
-        .or(`title.eq.${title}, author.eq.${title}`)
+        .or(`title.eq.%${title}%, author.eq.%${title}%`)
         .order('created_at', {ascending: false});
 
     if (error) {
