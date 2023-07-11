5be4c9d885eacec7ad624e39531a3ac67cb5e3d9
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-11 11:27:15 +0300

    Update SearchContent.tsx

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -46,8 +46,8 @@ const SearchContent: FC<SearchContent> = ({songs}) => {
 
     return (
         <>
-            <div className="flex items-center justify-items-start gap-x-4">
-                <h2 className="text-white text-4xl">Sort by:</h2><BsArrowDownUp size={26} onClick={toggleReverse} color={isReversed ? '#22c55e': 'rgb(163 163 163)'} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+            <div className="flex items-center justify-items-start gap-x-4 px-6 py-4">
+                <h2 className="text-white text-2xl">Sort by:</h2><BsArrowDownUp size={26} onClick={toggleReverse} color={isReversed ? '#22c55e': 'rgb(163 163 163)'} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
             <div className={twMerge(`flex flex-col gap-y-2 w-full px-6 h-full`,  player.activeId && "h-[calc(100%-130px)]")}>
                 {songs.map(song => (
