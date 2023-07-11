4129f28102a7aaa13b0d9ed1259b0d22bd06accf
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-11 18:40:44 +0300

    delete random

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -10,7 +10,6 @@ import {twMerge} from "tailwind-merge";
 import usePlayer from "@/hooks/usePlayer";
 import Loader from "@/components/Loader";
 import {BsArrowDownUp} from "react-icons/bs";
-import {FaArrowsTurnToDots} from "react-icons/fa6";
 
 interface LikedContentProps {
     songs: Song[];
@@ -22,16 +21,11 @@ const LikedContent: FC<LikedContentProps> = ({songs}) => {
     const router = useRouter();
     const {isLoading, user} = useUser();
     const player = usePlayer();
-
     const onPlay = useOnPlay(songs);
 
     const toggleReverse = (): void => {
         setIsReversed(prev => !prev);
         setSongsData(songsData.reverse());
-    }
-
-    const toggleRandom = (): void => {
-        setSongsData([...songsData].sort(() => Math.random() - 0.5));
     }
 
     useEffect((): void => {
@@ -57,7 +51,6 @@ const LikedContent: FC<LikedContentProps> = ({songs}) => {
         <>
             <div className="flex items-center justify-items-start gap-x-4 px-6 py-4">
                 <h2 className="text-white text-2xl">Sort by:</h2>
-                <FaArrowsTurnToDots size={26} onClick={toggleRandom} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
                 <BsArrowDownUp size={26} onClick={toggleReverse} color={isReversed ? '#22c55e': 'rgb(163 163 163)'} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
             <div className={twMerge(`flex flex-col gap-y-2 w-full px-6 h-full`, player.activeId && "h-[calc(100%-130px)]")}>
