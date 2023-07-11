8bce04d152d16f6a63b02cc36ea1fd4852457b43
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-11 16:18:06 +0300

    Update useLoadImageUser.ts

--- hooks/useLoadImageUser.ts
+++ hooks/useLoadImageUser.ts
@@ -1,12 +1,17 @@
 import {Song} from "@/types";
+import {useUser} from "@/hooks/useUser";
 import {useSupabaseClient} from "@supabase/auth-helpers-react";
 
 const useLoadImageUser = (userData: any) => {
     const supabaseClient = useSupabaseClient();
+    const user = useUser().user;
 
-    if (!userData) {
+    if (!userData && !user) {
         return null;
     }
+
+    console.log(user)
+    console.log(userData)
 
     const {data: imageData} = supabaseClient
         .storage
