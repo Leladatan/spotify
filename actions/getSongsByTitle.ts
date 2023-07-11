07032a0adaa94231ab9e4aac161eb83f1fa1c2bf
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-11 14:38:44 +0300

    author search

--- actions/getSongsByTitle.ts
+++ actions/getSongsByTitle.ts
@@ -15,7 +15,6 @@ const getSongsByTitle = async (title: string): Promise<Song[]> => {
     const {data, error} = await supabase
         .from('songs')
         .select('*')
-        .ilike('title', `%${title}%`)
         .ilike('author', `%${title}%`)
         .order('created_at', {ascending: false});
 
