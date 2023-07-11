cb09c44692536191db22d9d84ce09ae8584e8809
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-11 14:33:17 +0300

    Update SearchContent.tsx

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -55,7 +55,7 @@ const SearchContent: FC<SearchContent> = ({songs}) => {
                         flex items-center gap-x-4 w-full
                     "
                     >
-                        <div className="flex-1">
+                        <div className="xs:flex-1 xsm:w-[225px]">
                             <MediaItem data={song} onClick={(id: string) => onPlay(id)}/>
                         </div>
                         <LikeButton songId={song.id} />
