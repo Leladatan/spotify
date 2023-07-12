d027e9de26cd411422891b089e0319ca1ef77402
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-12 14:25:19 +0300

    Update getSongsByUserId.ts

--- actions/getSongsByUserId.ts
+++ actions/getSongsByUserId.ts
@@ -20,9 +20,9 @@ const getSongsByUserId = async (): Promise<Song[]> => {
     const {data, error} = await supabase
         .from('songs')
         .select('*')
+        .abortSignal(ac.signal)
         .eq('user_id', sessionData.session?.user.id)
-        .order('created_at', {ascending: false})
-        .abortSignal(ac.signal);
+        .order('created_at', {ascending: false});
 
     if (error) {
         console.log(error.message);
