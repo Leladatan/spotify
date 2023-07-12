f37c8253899e7f23029caa2f7af68419f152d5e8
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-12 11:26:22 +0300

    Update getSongsByTitle.ts

--- actions/getSongsByTitle.ts
+++ actions/getSongsByTitle.ts
@@ -9,14 +9,13 @@ const getSongsByTitle = async (title: string): Promise<Song[]> => {
     });
 
      if (!title) {
-        const allSongs = await getSongs();
-        return allSongs;
+        return await getSongs();
      }
 
     const {data, error} = await supabase
         .from('songs')
         .select('*')
-        .or(`title:ilike.%${title}%, author:ilike.%${title}%`)
+        .ilike('title', `%${title}%`)
         .order('created_at', {ascending: false});
 
     if (error) {
