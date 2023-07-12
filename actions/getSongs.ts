336c5c8606566f5f44c04fff067c58a150abee9d
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-12 15:19:24 +0300

    Update getSongs.ts

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -1,16 +1,22 @@
 import {Song} from "@/types";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from "next/headers";
+import {getPagination} from "@/functions";
+import {page} from "@/app/(site)/components/SongsContent";
 
 const getSongs = async (): Promise<Song[]> => {
     const supabase = createServerComponentClient({
         cookies: cookies
     });
 
+
+    const { from, to } = getPagination(page);
+
     const {data, error} = await supabase
         .from('songs')
         .select('*')
-        .order('created_at', {ascending: false});
+        .order('created_at', {ascending: false})
+        .range(from, to);
 
     if (error) {
         console.log(error);
