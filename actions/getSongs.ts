067dd6029e181861018b22795b18c4e61896135b
Vladimir <103734460+Leladatan@users.noreply.github.com>
2023-07-12 14:17:37 +0300

    Update getSongs.ts

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -2,20 +2,33 @@ import {Song} from "@/types";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from "next/headers";
 
-const getSongs = async (): Promise<Song[]> => {
+const getSongsByUserId = async (): Promise<Song[]> => {
     const supabase = createServerComponentClient({
         cookies: cookies
     });
 
+    const {data: sessionData, error: sessionError} = await supabase.auth.getSession();
+
+    if (sessionError) {
+        console.log(sessionError.message);
+        return [];
+    }
+
+    const ac:AbortController = new AbortController();
+    ac.abort();
+
     const {data, error} = await supabase
         .from('songs')
         .select('*')
+        .abortSignal(ac.signal)
+        .eq('user_id', sessionData.session?.user.id)
         .order('created_at', {ascending: false});
 
     if (error) {
-        console.log(error);
+        console.log(error.message);
     }
-    return data as any || [];
-}
 
-export default getSongs;
+    return data as any || [];
+};
+
+export default getSongsByUserId;
